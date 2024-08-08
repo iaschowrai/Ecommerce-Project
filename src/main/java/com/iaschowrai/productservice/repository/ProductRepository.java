90bd9245809f48e91a96508cc0dd76ffb4cd14c6
Irshad Ahmed <88178320+iaschowrai@users.noreply.github.com>
2024-08-07 22:56:21 -0400

    Delete src directory

--- src/main/java/com/iaschowrai/productservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.iaschowrai.productservice.repository;
-
-import com.iaschowrai.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
