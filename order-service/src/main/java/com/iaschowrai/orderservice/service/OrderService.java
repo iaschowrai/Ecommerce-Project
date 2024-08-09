3ce92c03ddb07936d1833212aa6ea3d29052a020
Irshad Ahmed <iaschowrai@gmail.com>
2024-08-08 23:51:24 -0400

    updated order service and configured the flyway migration with docker image mysql.

--- order-service/src/main/java/com/iaschowrai/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/iaschowrai/orderservice/service/OrderService.java
@@ -1,17 +1,12 @@
 package com.iaschowrai.orderservice.service;
 
-import com.iaschowrai.orderservice.dto.OrderLineItemsDto;
 import com.iaschowrai.orderservice.dto.OrderRequest;
 import com.iaschowrai.orderservice.model.Order;
-import com.iaschowrai.orderservice.model.OrderLineItems;
 import com.iaschowrai.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -26,8 +21,8 @@ public class OrderService {
        // map OrderRequest to Order object
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
         order.setSkuCode(orderRequest.skuCode());
+        order.setPrice(orderRequest.price());
         order.setQuantity(orderRequest.quantity());
 
         // save order to OrderRepository
--- order-service/src/test/java/com/iaschowrai/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/iaschowrai/orderservice/OrderServiceApplicationTests.java
@@ -1,13 +1,62 @@
 package com.iaschowrai.orderservice;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@Testcontainers
 class OrderServiceApplicationTests {
+	@Container
+	@ServiceConnection
+	static MySQLContainer<?> mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8.0.0"));
 
-	@Test
-	void contextLoads() {
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
 	}
 
+	static {
+		mySQLContainer.start();
+	}
+
+	@Test
+	void shouldSubmitOrder() {
+		String submitOrderJson = """
+            {
+                "skuCode": "iphone_15",
+                "price": 1000,
+                "quantity": 1
+            }
+            """;
+
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order/add")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+
+		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
+	}
 }
